60d9fa0738126d1a42c9440ba8f6212e2b30404d
babajide234 <babajide234@gmail.com>
2023-11-30 23:39:10 +0100

    fix: removed unused imports

--- src/components/Graph/index.tsx
+++ src/components/Graph/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 
 interface Balance {
     balance: string
--- src/route.tsx
+++ src/route.tsx
@@ -1,6 +1,5 @@
 import {createBrowserRouter} from "react-router-dom";
 import DashboardLayout from "./layouts/DashboardLayout";
-import Revenue from "./pages/Revenue";
 import App from "./App";
 
 export const router = createBrowserRouter([
